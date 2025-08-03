eaac2b20281633bf822366af2a9be794b05d983c
Khushi Sharma <kaushikkhushi110@gmail.com>
2025-08-03 12:34:33 +0530

    git commit -m "added sum"

---
+++ sum.java
@@ -0,0 +1,9 @@
+public class sum {
+    public static void main(String[] args) {
+        int a = 34;
+        int b = 45;
+        int c = 23;
+        int d = a + b + c;
+        System.out.println(d);
+    }
+}
