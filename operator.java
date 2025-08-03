cdc39fa6d14f572ba1fbbde3e8ff2aa8dcc1d9bf
Khushi Sharma <kaushikkhushi110@gmail.com>
2025-08-03 19:45:54 +0530

    git commit -m "added operator"

---
+++ operator.java
@@ -0,0 +1,22 @@
+public class operator {
+    public static void main(String[]args){
+        // 1. Arithmetic Operators
+        int a =4 ;
+        int b = 6 % a; //Modulo Operator
+
+    // 2. Assignment Operators
+    int c = 9;
+    c *= 3;
+    System.out.println(c);
+
+    //3. Comparison Operator
+    System.out.println(64<6);
+
+    //4. Logical Operator
+    System.out.println(64>5 && 64>98);
+    System.out.println(64>5 || 64>98);
+
+    //5. Bitwise Operator
+    System.out.println(2&3);
+    }
+}
